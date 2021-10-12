127f62fdf591ea13563e47138efade99f54eb1ee
abhishek@prophecy.io <abhishek@prophecy.io>
2021-10-12 21:27:59 +0000

    Workflow Code Updated

--- code/scala/workflows/ghgjhga/code/src/main/scala/main.scala
+++ code/scala/workflows/ghgjhga/code/src/main/scala/main.scala
@@ -11,8 +11,9 @@ import graph._
 object Main {
 
   def apply(spark: SparkSession): Unit = {
-    val df_source_0_out = source_0(spark)
-    val df_filter_0_out = filter_0(spark, df_source_0_out)
+    val df_source_0_out   = source_0(spark)
+    val df_filter_0_out   = filter_0(spark, df_source_0_out)
+    val df_reformat_0_out = reformat_0(spark)
   }
 
   def main(args: Array[String]): Unit = {
