4bc334ea1c0a1f8f6f3ea24327a0871be696a500
abhishek@prophecy.io <abhishek@prophecy.io>
2021-10-12 21:25:46 +0000

    Workflow Code Updated

--- code/scala/workflows/ghgjhga/code/src/main/scala/main.scala
+++ code/scala/workflows/ghgjhga/code/src/main/scala/main.scala
@@ -12,7 +12,7 @@ object Main {
 
   def apply(spark: SparkSession): Unit = {
     val df_source_0_out = source_0(spark)
-    val df_filter_0_out = filter_0(spark)
+    val df_filter_0_out = filter_0(spark, df_source_0_out)
   }
 
   def main(args: Array[String]): Unit = {
